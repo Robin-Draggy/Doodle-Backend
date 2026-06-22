7dc03b7fbb5b7685da35c80e9edd0275e8f60309
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-06-22 15:03:54 +0600

    fix

--- src/repositories/db.js
+++ src/repositories/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../config/constants.js";
 
+
 export const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)        
