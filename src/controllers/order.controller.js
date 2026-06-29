c1e7bf4f3af31c37b1e78bf65c52778c68e70553
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-06-29 15:50:09 +0600

    order status fixed

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -6,157 +6,66 @@ import {
   updateOrderStatusService,
   updatePaymentStatusService,
   cancelOrderService,
-} from "../services/order.service.js";
+} from '../services/order.service.js';
 
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { ApiResponse } from '../utils/ApiResponse.js';
+import { AsyncHandler } from '../utils/AsyncHandler.js';
 
 // Create Order
 
-export const createOrder = AsyncHandler(
-  async (req, res) => {
-    console.log("order data on controller:", req.body); // Debugging line
-    const order = await createOrderService({
-      userId: req.user._id,
-      ...req.body,
-    });
+export const createOrder = AsyncHandler(async (req, res) => {
+  const order = await createOrderService({
+    userId: req.user._id,
+    ...req.body,
+  });
 
-    return res
-      .status(201)
-      .json(
-        new ApiResponse(
-          201,
-          order,
-          "Order created successfully."
-        )
-      );
-  }
-);
+  return res.status(201).json(new ApiResponse(201, order, 'Order created successfully.'));
+});
 
 // Get My Orders
 
-export const getMyOrders = AsyncHandler(
-  async (req, res) => {
-    const orders =
-      await getMyOrdersService(
-        req.user._id
-      );
+export const getMyOrders = AsyncHandler(async (req, res) => {
+  const orders = await getMyOrdersService(req.user._id);
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          orders,
-          "Orders fetched successfully."
-        )
-      );
-  }
-);
+  return res.status(200).json(new ApiResponse(200, orders, 'Orders fetched successfully.'));
+});
 
 // Get Order By ID
 
-export const getOrderById = AsyncHandler(
-  async (req, res) => {
-    const order =
-      await getOrderByIdService(
-        req.params.orderId,
-        req.user
-      );
+export const getOrderById = AsyncHandler(async (req, res) => {
+  const order = await getOrderByIdService(req.params.orderId, req.user);
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          order,
-          "Order fetched successfully."
-        )
-      );
-  }
-);
+  return res.status(200).json(new ApiResponse(200, order, 'Order fetched successfully.'));
+});
 
 // Get All Orders
 
-export const getAllOrders = AsyncHandler(
-  async (req, res) => {
-    console.log("Fetching all orders in the controller..."); // Debugging line
-    const orders =
-      await getAllOrdersService();
+export const getAllOrders = AsyncHandler(async (req, res) => {
+  const orders = await getAllOrdersService();
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          orders,
-          "Orders fetched successfully."
-        )
-      );
-  }
-);
+  return res.status(200).json(new ApiResponse(200, orders, 'Orders fetched successfully.'));
+});
 
 // Update Order Status
 
-export const updateOrderStatus =
-  AsyncHandler(async (req, res) => {
-    console.log("checking req body in controller:", req.body); // Debugging line
-    const order =
-      await updateOrderStatusService(
-        req.params.orderId,
-        req.validatedData.orderStatus
-      );
+export const updateOrderStatus = AsyncHandler(async (req, res) => {
+  const order = await updateOrderStatusService(req.params.orderId, req.body.orderStatus);
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          order,
-          "Order status updated successfully."
-        )
-      );
-  });
+  return res.status(200).json(new ApiResponse(200, order, 'Order status updated successfully.'));
+});
 
 // Update Payment Status
 
-export const updatePaymentStatus =
-  AsyncHandler(async (req, res) => {
-    console.log("checking req body in controller:", req.body); // Debugging line
-    const order =
-      await updatePaymentStatusService(
-        req.params.orderId,
-        req.validatedData.paymentStatus
-      );
+export const updatePaymentStatus = AsyncHandler(async (req, res) => {
+  const order = await updatePaymentStatusService(req.params.orderId, req.body.paymentStatus);
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          order,
-          "Payment status updated successfully."
-        )
-      );
-  });
+  return res.status(200).json(new ApiResponse(200, order, 'Payment status updated successfully.'));
+});
 
 // Cancel Order
 
-export const cancelOrder =
-  AsyncHandler(async (req, res) => {
-    const order =
-      await cancelOrderService(
-        req.params.orderId,
-        req.user
-      );
+export const cancelOrder = AsyncHandler(async (req, res) => {
+  const order = await cancelOrderService(req.params.orderId, req.user);
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          order,
-          "Order cancelled successfully."
-        )
-      );
-  });
+  return res.status(200).json(new ApiResponse(200, order, 'Order cancelled successfully.'));
+});
